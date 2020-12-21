37e7a7e5742995d22d01660b93c0b3f56e77ac1d
Pedro Dias <pedrofelipe18@hotmail.com.br>
2020-12-21 15:53:13 -0300

    Up

---
+++ MongoDB-API/Interfaces/IEventoRepository.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB_API_Domains;
+using System.Collections.Generic;
+
+namespace MongoDB_API_Interfaces
+{
+    public interface IEventoRepository
+    {
+        List<EventoDomain> Listar();
+        EventoDomain BuscarPorId(string id);
+
+        void Adcionar(EventoDomain evento);
+
+        void Atualizar(string id, EventoDomain evento);
+
+        void Remover(string id);
+    }
+}
---
+++ MongoDB-API/Repositories/EventoRepository.cs
@@ -0,0 +1,88 @@
+﻿using MongoDB.Driver;
+using MongoDB_API_Interfaces;
+using MongoDB_API_Context;
+using System;
+using System.Collections.Generic;
+using MongoDB_API_Domains;
+
+namespace Nyous.Api.NoSql.Repositories
+{
+    public class EventoRepository : IEventoRepository
+    {
+        private readonly IMongoCollection<EventoDomain> _eventos;
+
+        public EventoRepository(INyousDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _eventos = database.GetCollection<EventoDomain>(settings.EventosCollectionName);
+        }
+
+        public void Adcionar(EventoDomain evento)
+        {
+            try
+            {
+                _eventos.InsertOne(evento);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Atualizar(string id, EventoDomain evento)
+        {
+            try
+            {
+                _eventos.ReplaceOne(c => c.Id == id, evento);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public EventoDomain BuscarPorId(string id)
+        {
+            try
+            {
+                return _eventos.Find<EventoDomain>(e => e.Id == id).First();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<EventoDomain> Listar()
+        {
+            try
+            {
+                return _eventos.AsQueryable<EventoDomain>().ToList();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Remover(string id)
+        {
+            try
+            {
+                _eventos.Find<EventoDomain>(e => e.Id == id).First();
+
+                _eventos.DeleteOne(c => c.Id == id);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
